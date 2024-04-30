707e5cea1b7f47cbec4d79fad07f27fb6883bdc1
enzogit <enzo_91_10@hotmail.com>
2024-04-30 05:06:38 -0300

    tratando de arreglar el carts router

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -4,17 +4,17 @@ const cartRouter = express.Router();
 import CartManager from "../controllers/carts.manager.js";
 import CartModel from "../models/carts.model.js";
 
-//Visualizar todos los carritos existentes
+//Visualizar todos los carritos existentes //Funciona
 cartRouter.get("/api/carts/", async (req, res) => {
     const carts = await CartModel.find().lean();
     res.render("cartsContainer", { carts: carts })
 })
 //Visualizar un solo carrito
 cartRouter.get("/api/carts/:cid", async (req, res) => {
-    let { cartId } = req.params
+    let cartId  = req.params.cid
     try {
-        const cart = await CartModel.findById({ _id: cartId })
-        res.render("cartsContainer", { carts: cart })
+        const cart = await CartModel.findById(cartId)
+        res.render("cartsContainer", { carts:cart })
         if (!cartId) {
             console.log("No existe un carrito con ese Id")
             res.send.json("No existe un carrito con ese Id")
@@ -64,9 +64,10 @@ cartRouter.put("/api/carts/:cid/products/:pid", async (req, res) => {
 })
 //Eliminar todos los productos del carrito seleccionado
 cartRouter.delete("/api/carts/:cid", async (req, res) => {
-    let { cid } = req.params
+    let cid  = req.params.cid
     try {
-        const deleteCart = await CartManager.findByIdandDelete({ _id: cid })
+        await CartManager.findByIdandDelete(cid)
+        await carts.save();
         res.json({ message: "Carrito eliminado con éxito" })
     } catch (error) {
         res.status(500).json({ message: "Error: No se pudo actualizar la cantidad" })
